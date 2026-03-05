55714bb3e14f63ec046c5a1bc76d05ace117eb58
Pedro Melo <pmelo2005@me.com>
2026-03-04 16:14:11 -0800

    creating new scene, adding files

---
+++ Assets/Scripts/Coin.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+    public void Collect()
+    {
+        // Play sound
+        // play animation
+        // destroy
+        Destroy(this);
+    }
+}
---
+++ Assets/Scripts/CoinCollector.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class CoinCollector : MonoBehaviour
+{
+    public int currentCoins = 0;
+
+    private void OnTriggerEnter(Collider other) 
+    {
+        Coin possibleCoin = other.GetComponent<Coin>();
+
+        if(possibleCoin != null)
+        {
+            possibleCoin.Collect();
+            currentCoins++;
+        }
+
+        if(other.TryGetComponent<Coin>(out Coin _possibleCoin))
+        {
+            possibleCoin.Collect();
+            currentCoins++;
+        }
+    }
+
+    // private void OnGUI()
+    // {
+    //     GUILayout.Label($"COINS: {currentCoins}");
+    // }
+}
